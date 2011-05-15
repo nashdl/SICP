142fe5d856b10b0cdae62be00d22c17f2d83e3ce
Calvin Bottoms <calvin.bottoms@gmail.com>
2011-05-15 18:34:55 -0500

    Added 1.10.

---
+++ 1.10.scm
@@ -0,0 +1,21 @@
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (- x 1)
+                 (A x (- y 1))))))
+
+;(A 1 10)
+;1024
+
+;(A 2 4)
+;65536
+
+;(A 3 3)
+;65536
+
+(define (f n) (A 0 n)) ;f(n) = 2n
+
+(define (g n) (A 1 n)) ;g(n) = 2^n
+
+(define (h n) (A 2 n)) ;h(n) = 2^(h(n-1))
