0a1f9d5c493598c27770e932e840a5d17542c448
Calvin Bottoms <calvin.bottoms@gmail.com>
2011-05-20 04:26:43 -0500

    Added 1.16.

---
+++ 1.16.scm
@@ -0,0 +1,6 @@
+(define (fast-expt b n)
+  (define (fast-expt-iter b n a)
+    (cond ((= n 0) a)
+          ((even? n) (fast-expt-iter (* b b) (/ n 2) a))
+          (else (fast-expt-iter b (- n 1) (* a b)))))
+  (fast-expt-iter b n 1))
