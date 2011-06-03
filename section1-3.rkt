5b2ca28439bcf661a9b8db2b75c81fc0072500dd
calebphillips <caleb.phillips@gmail.com>
2011-06-02 21:20:00 -0500

    Added iterative sum

--- section1-3.rkt
+++ section1-3.rkt
@@ -1,14 +1,23 @@
 
 #lang racket
 
-(define (sum term a next b)
-(if (> a b)
-  0
-  (+ (term a)
-    (sum term (next a) next b)))) 
+(define (sum-recur term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b)))) 
 
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))
+      
 (define (cube x)
   (* x x x))
+
+(define (identity x) x)
 
 (define (inc x) (+ x 1))
 
