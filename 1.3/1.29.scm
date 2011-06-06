09fef40875b36a842a64339827266b426fd128cb
Calvin Bottoms <calvin.bottoms@gmail.com>
2011-06-05 21:20:09 -0500

    Extracted term function.

--- 1.3/1.29.scm
+++ 1.3/1.29.scm
@@ -3,16 +3,19 @@
 (define (simpson f a b n)
   (define h (/ (- b a) n))
   (define (y k) (f (+ a (* k h))))
+  (define (term i)
+    (cond ((= i 0) (y i))
+          ((= i n) (y i))
+          ((odd? i) (* 4 (y i)))
+          (else (* 2 (y i)))))
   (define (iter i)
-    (if (= i n)
-        (y i)
-        (+ (* 4 (y (- i 1)))
-           (* 2 (y i))
-           (iter (+ i 2)))))
+    (if (> i n)
+        0.0
+        (+ (term i)
+           (iter (+ i 1)))))
   (* (/ h 3.0)
-     (+ (y 0)
-        (iter 2))))
+     (iter 0)))
   
-(display (simpson cube 0 1 100))  ; 0.23706268
+(display (simpson cube 0 1 100))  ; 0.24999999999999992
 (display "\n")
-(display (simpson cube 0 1 1000)) ; 0.248670662668
+(display (simpson cube 0 1 1000)) ; 0.2500000000000002
