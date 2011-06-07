630d62aa115bad31d3639298bc7a1073d69477ba
Calvin Bottoms <calvin.bottoms@gmail.com>
2011-06-06 21:12:14 -0500

    Plugged in sum function.

--- 1.3/1.29.scm
+++ 1.3/1.29.scm
@@ -1,5 +1,13 @@
 (define (cube x) (* x x x))
 
+(define (inc n) (+ n 1))
+
+(define (sum term a next b)
+  (if (> a b)
+      0.0
+      (+ (term a)
+         (sum term (next a) next b))))
+
 (define (simpson f a b n)
   (define h (/ (- b a) n))
   (define (y k) (f (+ a (* k h))))
@@ -8,14 +16,9 @@
           ((= i n) (y i))
           ((odd? i) (* 4 (y i)))
           (else (* 2 (y i)))))
-  (define (iter i)
-    (if (> i n)
-        0.0
-        (+ (term i)
-           (iter (+ i 1)))))
   (* (/ h 3.0)
-     (iter 0)))
+     (sum term 0 inc n)))
   
-(display (simpson cube 0 1 100))  ; 0.24999999999999992
-(display "\n")
-(display (simpson cube 0 1 1000)) ; 0.2500000000000002
+; (display (simpson cube 0 1 100))  ; 0.24999999999999992
+; (display "\n")
+; (display (simpson cube 0 1 1000)) ; 0.2500000000000002
