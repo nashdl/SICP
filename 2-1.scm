db19ecef3b24f5c6c332112840ba8b70a83b83a4
calebphillips <caleb.phillips@gmail.com>
2011-06-16 16:44:10 -0500

    a little cleaner on 2.1

--- 2-1.scm
+++ 2-1.scm
@@ -2,12 +2,12 @@
 
 (require "test.scm")
 
-(define (make-rat n d) 
-  ;(define (both pred pair) (and (pred (car pair)) (pred (cdr pair))))
+(define (make-rat n d)   
+  (define (same-sign?) (or (and (negative? n) (negative? d)) (and (positive? d) (positive? n))))
   (let ((g (gcd n d))
-        (same-sign? (or (and (negative? n) (negative? d)) (and (positive? d) (positive? n)))))    
-    (cons (/ (if same-sign? (abs n) (- (abs n))) g) 
-          (abs (/ d g)))))
+        (numer-op (if (same-sign?) abs (lambda (x) (- (abs x))))))    
+    (cons (/ (numer-op n) g) 
+          (/ (abs d) g))))
 
 (define (numer x) (car x))
 
